538b084071510234971915146d81969f5b16b563
Daryl Wagoner <wa1gon@arrl.net>
2001-03-06 21:42:12 +0000

    test program test date check function.

---
+++ chkdate.cc
@@ -0,0 +1,14 @@
+#include "amcert.h"
+#include "sign.h"
+
+#include <stdio.h>
+int debugLevel=1;
+int main(int argc,char *argv[])
+{
+
+  if (argc != 2)
+    return(1);
+  fprintf(stderr,"chkDate %s\n",argv[1]);
+  printf("chkDate returned %d\n",chkDate(argv[1]));
+  return(0);
+}
