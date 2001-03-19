e59a791d939f4e37a2839c32b97c42df7ba530ed
Daryl Wagoner <wa1gon@arrl.net>
2001-03-19 12:57:24 +0000

    change to get genkey to work

--- tqsl/genkeys.cpp
+++ tqsl/genkeys.cpp
@@ -1,9 +1,12 @@
 //---------------------------------------------------------------------------
 #include <vcl.h>
+#include <fcntl.h>
+#include<io.h>
+
 #pragma hdrstop
 
 #include "genkeys.h"
-#include <tqsl.h>
+#include "../tqsl.h"
 //---------------------------------------------------------------------------
 #pragma package(smart_init)
 #pragma resource "*.dfm"
@@ -18,18 +21,56 @@ __fastcall TgenKeyFm::TgenKeyFm(TComponent* Owner)
 void __fastcall TgenKeyFm::Button1Click(TObject *Sender)
 {
  char  callsign[100];
- const char *secretFile = "c:\\src\\tpriv.prv";
- const char *pubFile = "c:\\src\\tpriv.pub";
+ //const char *secretFile = "c:\\src\\tpriv.prv";
+ //const char *pubFile = "c:\\src\\tpriv.pub";
+ const char *secretFile = "tpriv.prv";
+ const char *pubFile = "tpriv.pub";
+ TqslPublicKey pubKey;
+ char		*secretKey;
 
  int rc;
   if (callSignEd->Text.Length() < 3)
    return;
-  rc = tqslGenNewKeys(callSignEd->Text.c_str(),secretFile,pubFile);
-  if (rc <= 0)
+  rc = tqslGenNewKeys(callSignEd->Text.c_str(),&secretKey,&pubKey);
+  if (rc > 0)
     {
      Label2->Caption = rc;
-      //fprintf(stderr,"Creation of public/private key pair failed! %d\n",rc);
-      //return(rc);
-    } 
+     int fd;
+
+     fd = open(pubFile,O_WRONLY|O_CREAT|O_TRUNC,0644);
+     if (fd >= 0)
+       {
+         rc = write(fd,&pubKey,sizeof(pubKey));
+         close(fd);
+         if (rc != sizeof(pubKey))
+	          {
+	            //fprintf(stderr,"writing public file failed\n");
+	            return;
+	          }
+       }
+     else
+       {
+         //fprintf(stderr,"creating public file failed\n");
+         return;
+       }
+
+  fd = open(secretFile,O_WRONLY|O_CREAT|O_TRUNC,0600);
+  if (fd >= 0)
+    {
+      rc = write(fd,secretKey,strlen(secretKey));
+      close(fd);
+      if (rc != (int)strlen(secretKey))
+	      {
+	      //fprintf(stderr,"writing private file failed\n");
+	      return;
+	      }
+    }
+  else
+    {
+      //fprintf(stderr,"creating private file failed\n");
+      return;
+    }	 
+
+  } 
 }
 //---------------------------------------------------------------------------
