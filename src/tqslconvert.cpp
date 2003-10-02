54d34deb72e64333468d9ea13e90694782394d1d
Jon Bloom <ke3z@arrl.net>
2003-10-02 10:24:30 +0000

    V1.4

--- src/tqslconvert.cpp
+++ src/tqslconvert.cpp
@@ -53,7 +53,10 @@ public:
 	bool allow_bad_calls;
 	set <string> modes;
 	set <string> bands;
+	set <string> propmodes;
+	set <string> satellites;
 	string rec_text;
+	tQSL_Date start, end;
 };
 
 inline TQSL_CONVERTER::TQSL_CONVERTER()  : sentinel(0x4445) {
@@ -67,6 +70,8 @@ inline TQSL_CONVERTER::TQSL_CONVERTER()  : sentinel(0x4445) {
 	rec_done = true;
 	allow_bad_calls = false;
 	memset(&rec, 0, sizeof rec);
+	memset(&start, 0, sizeof start);
+	memset(&end, 0, sizeof end);
 	// Init the band data
 	const char *val;
 	int n = 0;
@@ -85,6 +90,24 @@ inline TQSL_CONVERTER::TQSL_CONVERTER()  : sentinel(0x4445) {
 		tqsl_getMode(i, &val, 0);
 		if (val)
 			modes.insert(val);
+	}
+	// Init the propagation mode data
+	n = 0;
+	tqsl_getNumPropagationMode(&n);
+	for (int i = 0; i < n; i++) {
+		val = 0;
+		tqsl_getPropagationMode(i, &val, 0);
+		if (val)
+			propmodes.insert(val);
+	}
+	// Init the satellite data
+	n = 0;
+	tqsl_getNumSatellite(&n);
+	for (int i = 0; i < n; i++) {
+		val = 0;
+		tqsl_getSatellite(i, &val, 0, 0, 0);
+		if (val)
+			satellites.insert(val);
 	}
 }
 
@@ -138,12 +161,16 @@ check_conv(tQSL_Converter conv) {
 }
 
 static tqsl_adifFieldDefinitions adif_qso_record_fields[] = {
-	{ "CALL", "", TQSL_ADIF_RANGE_TYPE_NONE, 13, 0, 0, NULL },
-	{ "BAND", "", TQSL_ADIF_RANGE_TYPE_NONE, 5, 0, 0, NULL },
-	{ "MODE", "", TQSL_ADIF_RANGE_TYPE_NONE, 16, 0, 0, NULL },
+	{ "CALL", "", TQSL_ADIF_RANGE_TYPE_NONE, TQSL_CALLSIGN_MAX, 0, 0, NULL },
+	{ "BAND", "", TQSL_ADIF_RANGE_TYPE_NONE, TQSL_BAND_MAX, 0, 0, NULL },
+	{ "MODE", "", TQSL_ADIF_RANGE_TYPE_NONE, TQSL_MODE_MAX, 0, 0, NULL },
 	{ "QSO_DATE", "", TQSL_ADIF_RANGE_TYPE_NONE, 10, 0, 0, NULL },
 	{ "TIME_ON", "", TQSL_ADIF_RANGE_TYPE_NONE, 10, 0, 0, NULL },
-	{ "FREQ", "", TQSL_ADIF_RANGE_TYPE_NONE, 15, 0, 0, NULL },
+	{ "FREQ", "", TQSL_ADIF_RANGE_TYPE_NONE, TQSL_FREQ_MAX, 0, 0, NULL },
+	{ "FREQ_RX", "", TQSL_ADIF_RANGE_TYPE_NONE, TQSL_FREQ_MAX, 0, 0, NULL },
+	{ "BAND_RX", "", TQSL_ADIF_RANGE_TYPE_NONE, TQSL_BAND_MAX, 0, 0, NULL },
+	{ "SAT_NAME", "", TQSL_ADIF_RANGE_TYPE_NONE, TQSL_SATNAME_MAX, 0, 0, NULL },
+	{ "PROP_MODE", "", TQSL_ADIF_RANGE_TYPE_NONE, TQSL_PROPMODE_MAX, 0, 0, NULL },
 	{ "eor", "", TQSL_ADIF_RANGE_TYPE_NONE, 0, 0, 0, NULL },
 };
 
@@ -235,14 +262,47 @@ find_matching_cert(TQSL_CONVERTER *conv) {
 
 static const char *notypes[] = { "D","T","M","N","C","" };
 
-/*
-static char *
-tqsl_strdup(const char *str) {
-	char *cp = new char[strlen(str)+1];
-	strcpy(cp, str);
-	return cp;
+static const char *
+tqsl_infer_band(const char* infreq) {
+	double freq = atof(infreq);
+	double freq_khz = freq * 1000.0;
+	int nband = 0;
+	tqsl_getNumBand(&nband);
+	for (int i = 0; i < nband; i++) {
+		const char *name;
+		const char *spectrum;
+		int low, high;
+		if (tqsl_getBand(i, &name, &spectrum, &low, &high))
+			break;
+		bool match = false;
+		if (!strcmp(spectrum, "HF")) {
+			if (freq_khz >= low && freq_khz <= high)
+				match = true;
+		} else {
+			if (freq >= low && freq <= high)
+				match = true;
+		}
+		if (match)
+			 return name;
+	}
+	return "";
 }
-*/
+
+DLLEXPORT int
+tqsl_setADIFConverterDateFilter(tQSL_Converter convp, tQSL_Date *start, tQSL_Date *end) {
+	TQSL_CONVERTER *conv;
+	if (!(conv = check_conv(convp)))
+		return 1;
+	if (start == NULL)
+		conv->start.year = conv->start.month = conv->start.day = 0;
+	else
+		conv->start = *start;
+	if (end == NULL)
+		conv->end.year = conv->end.month = conv->end.day = 0;
+	else
+		conv->end = *end;
+	return 0;
+}
 
 DLLEXPORT const char *
 tqsl_getConverterGABBI(tQSL_Converter convp) {
@@ -277,6 +337,16 @@ tqsl_getConverterGABBI(tQSL_Converter convp) {
 					strncpy(conv->rec.mode, (char *)(result.data), sizeof conv->rec.mode);
 				} else if (!strcasecmp(result.name, "FREQ") && result.data) {
 					strncpy(conv->rec.freq, (char *)(result.data), sizeof conv->rec.freq);
+				} else if (!strcasecmp(result.name, "FREQ") && result.data) {
+					strncpy(conv->rec.freq, (char *)(result.data), sizeof conv->rec.freq);
+				} else if (!strcasecmp(result.name, "FREQ_RX") && result.data) {
+					strncpy(conv->rec.rxfreq, (char *)(result.data), sizeof conv->rec.rxfreq);
+				} else if (!strcasecmp(result.name, "BAND_RX") && result.data) {
+					strncpy(conv->rec.rxband, (char *)(result.data), sizeof conv->rec.rxband);
+				} else if (!strcasecmp(result.name, "SAT_NAME") && result.data) {
+					strncpy(conv->rec.satname, (char *)(result.data), sizeof conv->rec.satname);
+				} else if (!strcasecmp(result.name, "PROP_MODE") && result.data) {
+					strncpy(conv->rec.propmode, (char *)(result.data), sizeof conv->rec.propmode);
 				} else if (!strcasecmp(result.name, "QSO_DATE") && result.data) {
 					cstat = tqsl_initDate(&(conv->rec.date), (const char *)result.data);
 				} else if (!strcasecmp(result.name, "TIME_ON") && result.data) {
@@ -300,32 +370,11 @@ tqsl_getConverterGABBI(tQSL_Converter convp) {
 				tQSL_Error = TQSL_ADIF_ERROR;
 				return 0;
 			}
-			// ADIF record is complete. See if we need to infer the BAND field.
-			if (conv->rec.band[0] == 0) {
-				double freq = atof(conv->rec.freq);
-				double freq_khz = freq * 1000.0;
-				int nband = 0;
-				tqsl_getNumBand(&nband);
-				for (int i = 0; i < nband; i++) {
-					const char *name;
-					const char *spectrum;
-					int low, high;
-					if (tqsl_getBand(i, &name, &spectrum, &low, &high))
-						break;
-					bool match = false;
-					if (!strcmp(spectrum, "HF")) {
-						if (freq_khz >= low && freq_khz <= high)
-							match = true;
-					} else {
-						if (freq >= low && freq <= high)
-							match = true;
-					}
-					if (match) {
-						strncpy(conv->rec.band, name, sizeof conv->rec.band);
-						break;
-					}
-				}
-			}
+			// ADIF record is complete. See if we need to infer the BAND fields.
+			if (conv->rec.band[0] == 0)
+				strncpy(conv->rec.band, tqsl_infer_band(conv->rec.freq), sizeof conv->rec.band);
+			if (conv->rec.rxband[0] == 0)
+				strncpy(conv->rec.rxband, tqsl_infer_band(conv->rec.rxfreq), sizeof conv->rec.rxband);
 		} else if (conv->cab) {
 			TQSL_CABRILLO_ERROR_TYPE stat;
 			do {
@@ -360,6 +409,20 @@ tqsl_getConverterGABBI(tQSL_Converter convp) {
 			return 0;
 		}
 	}
+	// Check QSO date against user-specified date range.
+	if (tqsl_isDateValid(&(conv->rec.date))) {
+		if (tqsl_isDateValid(&(conv->start)) && tqsl_compareDates(&(conv->rec.date), &(conv->start)) < 0) {
+			conv->rec_done = true;
+			tQSL_Error = TQSL_DATE_OUT_OF_RANGE;
+			return 0;
+		}
+		if (tqsl_isDateValid(&(conv->end)) && tqsl_compareDates(&(conv->rec.date), &(conv->end)) > 0) {
+			conv->rec_done = true;
+			tQSL_Error = TQSL_DATE_OUT_OF_RANGE;
+			return 0;
+		}
+	}
+
 	// Do field value mapping
 	tqsl_strtoupper(conv->rec.callsign);
 	if (!conv->allow_bad_calls) {
@@ -389,15 +452,43 @@ tqsl_getConverterGABBI(tQSL_Converter convp) {
 		tQSL_Error = TQSL_CUSTOM_ERROR;
 		return 0;
 	}
+	if (conv->rec.propmode[0] != '\0'
+		&& conv->propmodes.find(conv->rec.propmode) == conv->propmodes.end()) {
+		conv->rec_done = true;
+		snprintf(tQSL_CustomError, sizeof tQSL_CustomError, "Invalid PROP_MODE (%s)", conv->rec.propmode);
+		tQSL_Error = TQSL_CUSTOM_ERROR;
+		return 0;
+	}
+	if (conv->rec.satname[0] != '\0'
+		&& conv->satellites.find(conv->rec.satname) == conv->satellites.end()) {
+		conv->rec_done = true;
+		snprintf(tQSL_CustomError, sizeof tQSL_CustomError, "Invalid SAT_NAME (%s)", conv->rec.satname);
+		tQSL_Error = TQSL_CUSTOM_ERROR;
+		return 0;
+	}
+	if (!strcmp(conv->rec.propmode, "SAT") && conv->rec.satname[0] == '\0') {
+		conv->rec_done = true;
+		snprintf(tQSL_CustomError, sizeof tQSL_CustomError, "PROP_MODE = 'SAT' but no SAT_NAME");
+		tQSL_Error = TQSL_CUSTOM_ERROR;
+		return 0;
+	}
+	if (strcmp(conv->rec.propmode, "SAT") && conv->rec.satname[0] != '\0') {
+		conv->rec_done = true;
+		snprintf(tQSL_CustomError, sizeof tQSL_CustomError, "SAT_NAME set but PROP_MODE is not 'SAT'");
+		tQSL_Error = TQSL_CUSTOM_ERROR;
+		return 0;
+	}
 
 	// Check cert
 	if (conv->ncerts <= 0) {
+		conv->rec_done = true;
 		tQSL_Error = TQSL_CERT_NOT_FOUND;
 		return 0;
 	}
 
 	int cidx = find_matching_cert(conv);
 	if (cidx < 0) {
+		conv->rec_done = true;
 		tQSL_Error = TQSL_CERT_DATE_MISMATCH;
 		return 0;
 	}
