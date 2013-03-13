bb339f57113b4ca2c2ab4401ae3a3890178267e0
Rick Murphy <k1mu@arrl.net>
2013-03-13 09:44:55 -0400

    Use mktemp for staging dir, overwrite existing dmg

--- osx_createdmg.sh
+++ osx_createdmg.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
 TQSLLIBPATH=`pwd`/src/libtqsllib.dylib
+WORKDIR=`mktemp -d /tmp/tqsl.XXXXX` || exit 1
 
 /bin/echo -n "Copying files to image directory... "
 
-mkdir tmp-dmg
-cp apps/ChangeLog.txt tmp-dmg/ChangeLog.txt
-cp LICENSE.txt tmp-dmg/
-cp apps/quick "tmp-dmg/Quick Start.txt"
-mkdir tmp-dmg/TrustedQSL
-cp -r apps/tqsl.app tmp-dmg/TrustedQSL
-cp -r apps/tqslcert.app tmp-dmg/TrustedQSL
+mkdir $WORKDIR
+cp apps/ChangeLog.txt $WORKDIR/ChangeLog.txt
+cp LICENSE.txt $WORKDIR/
+cp apps/quick "$WORKDIR/Quick Start.txt"
+mkdir $WORKDIR/TrustedQSL
+cp -r apps/tqsl.app $WORKDIR/TrustedQSL
+cp -r apps/tqslcert.app $WORKDIR/TrustedQSL
 
 /bin/echo "done"
 
@@ -18,25 +19,25 @@ cp -r apps/tqslcert.app tmp-dmg/TrustedQSL
 
 for app in tqsl tqslcert
 do
-    cp $TQSLLIBPATH tmp-dmg/TrustedQSL/$app.app/Contents/MacOS
-    install_name_tool -change $TQSLLIBPATH @executable_path/libtqsllib.dylib tmp-dmg/TrustedQSL/$app.app/Contents/MacOS/$app
-    cp src/config.xml tmp-dmg/TrustedQSL/$app.app/Contents/Resources
+    cp $TQSLLIBPATH $WORKDIR/TrustedQSL/$app.app/Contents/MacOS
+    install_name_tool -change $TQSLLIBPATH @executable_path/libtqsllib.dylib $WORKDIR/TrustedQSL/$app.app/Contents/MacOS/$app
+    cp src/config.xml $WORKDIR/TrustedQSL/$app.app/Contents/Resources
 done
 
 /bin/echo "done"
 
 /bin/echo -n "Installing the help... "
 
-cp -r apps/help/tqslapp tmp-dmg/TrustedQSL/tqsl.app/Contents/Resources/Help
-cp -r apps/help/tqslcert tmp-dmg/TrustedQSL/tqslcert.app/Contents/Resources/Help
+cp -r apps/help/tqslapp $WORKDIR/TrustedQSL/tqsl.app/Contents/Resources/Help
+cp -r apps/help/tqslcert $WORKDIR/TrustedQSL/tqslcert.app/Contents/Resources/Help
 
 /bin/echo "done"
 
 /bin/echo "Creating the disk image..."
 
 #hdiutil uses dots to show progress
-hdiutil create -srcfolder tmp-dmg -volname "TrustedQSL v1.14" tqsl-114.dmg
+hdiutil create -ov -srcfolder $WORKDIR -volname "TrustedQSL v1.14" tqsl-114.dmg
 
 /bin/echo -n "Cleaning up temporary files.. "
-rm -r tmp-dmg
+rm -r $WORKDIR
 /bin/echo "Finished!"
