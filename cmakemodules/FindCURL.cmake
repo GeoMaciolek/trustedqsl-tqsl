1457a2f7f3b3e0c760d25378a3bf1fa3bc965d6c
kc2ywe <kc2ywe@gmail.com>
2013-06-25 19:48:15 -0400

    add curl 7.31 to findcurl search path; reorder to prefer higher versions

--- cmakemodules/FindCURL.cmake
+++ cmakemodules/FindCURL.cmake
@@ -24,8 +24,9 @@
 FIND_PATH(CURL_INCLUDE_DIR curl/curl.h
 	#for windows, there has to be a better way
 	"C:\\curl\\include\\"
-	"C:\\curl-7.28.1\\builds\\libcurl-vc-x86-release-static-ipv6-sspi-spnego-winssl\\include"
+	"C:\\curl-7.31.0\\builds\\libcurl-vc-x86-release-static-ipv6-sspi-spnego-winssl\\include"
 	"C:\\curl-7.29.0\\builds\\libcurl-vc-x86-release-static-ipv6-sspi-spnego-winssl\\include"
+	"C:\\curl-7.28.1\\builds\\libcurl-vc-x86-release-static-ipv6-sspi-spnego-winssl\\include"
 )
 MARK_AS_ADVANCED(CURL_INCLUDE_DIR)
 
@@ -39,8 +40,9 @@ FIND_LIBRARY(CURL_LIBRARY NAMES
     libcurl_imp
     curllib_static
 	PATHS
-	"C:\\curl-7.28.1\\builds\\libcurl-vc-x86-release-static-ipv6-sspi-spnego-winssl\\lib" #winssl
+	"C:\\curl-7.31.0\\builds\\libcurl-vc-x86-release-static-ipv6-sspi-spnego-winssl\\lib" #winssl
 	"C:\\curl-7.29.0\\builds\\libcurl-vc-x86-release-static-ipv6-sspi-spnego-winssl\\lib" #winssl
+	"C:\\curl-7.28.1\\builds\\libcurl-vc-x86-release-static-ipv6-sspi-spnego-winssl\\lib" #winssl
 	"C:\\curl\\lib\\"
 	"C:\\curl-7.28.1\\lib\\"
 )
