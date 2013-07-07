6c15b3bc897a186237310411745f54c4f5b00589
kc2ywe <kc2ywe@gmail.com>
2013-07-07 15:53:58 -0400

    use an XML comment instead of an element for the warning

--- apps/tqsl.cpp
+++ apps/tqsl.cpp
@@ -2781,8 +2781,8 @@ MyFrame::BackupConfig(wxString& filename, bool quiet) {
 		TQSLConfig* conf = new TQSLConfig();
 
 		gzprintf(out, "<TQSL_Configuration>\n");
-		gzprintf(out, "<Warning>If you directly edit this file, you are responsible for its content.\n");
-		gzprintf(out, "The ARRL's LoTW Help Desk will be unable to assist you.</Warning>\n");
+		gzprintf(out, "<!-- Warning! If you directly edit this file, you are responsible for its content.\n");
+		gzprintf(out, "The ARRL's LoTW Help Desk will be unable to assist you. -->\n");
 		gzprintf(out, "<Certificates>\n");
 
 		if (!quiet) {
