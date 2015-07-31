f904df5d59a0a7f76cecfcbdc9e18d8b37dc310b
Rick Murphy <k1mu@arrl.net>
2015-07-31 12:41:32 -0400

    Fix OSX compiler warning

--- apps/tqsl.cpp
+++ apps/tqsl.cpp
@@ -4676,7 +4676,7 @@ QSLApp::OnInit() {
 	for (int i = 1; i < argc; i++) {
 		origCommandLine += wxT(" ");
 		origCommandLine += argv[i];
-		if (argv[i][0] == wxT('-') || argv[i][0] == wxT('/'))
+		if (argv[i] && (argv[i][0] == wxT('-') || argv[i][0] == wxT('/')))
 			if (wxIsalpha(argv[i][1]) && wxIsupper(argv[i][1]))
 				argv[i][1] = wxTolower(argv[i][1]);
 	}
