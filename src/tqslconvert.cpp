8089b3c38d797630c0abc2fa90b38df0685f9a89
Rick Murphy <k1mu@arrl.net>
2015-12-09 00:38:55 -0500

    Fix logic error in DB_PRIVATE flag handling

--- src/tqslconvert.cpp
+++ src/tqslconvert.cpp
@@ -550,7 +550,7 @@ static bool open_db(TQSL_CONVERTER *conv, bool readonly) {
 				fprintf(conv->errfile, "opening DB %s returns status %s\n", conv->dbpath, db_strerror(dbret));
 			// Can't open the database - maybe try private?
 			if ((dbret == EACCES || dbret == EROFS) || (dbret == EINVAL && errno == dbret)) {
-				if (!(envflags && DB_PRIVATE)) {
+				if (!(envflags & DB_PRIVATE)) {
 					envflags |= DB_PRIVATE;
 					continue;
 				}
