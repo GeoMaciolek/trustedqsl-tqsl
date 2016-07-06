59bcc25eab975fef4bf53663afed464a11fea4fe
Rick Murphy <k1mu@arrl.net>
2016-07-05 20:36:04 -0400

    Update version to 2.3

--- apps/makepot.sh
+++ apps/makepot.sh
@@ -7,5 +7,5 @@
 xgettext --c++ --default-domain=tqslapp --output=tqslapp.pot \
 	 --keyword=_  --keyword=__ --keyword=i18narg \
 	 --escape --copyright-holder="The TrustedQSL Developers" \
-	 --package-name="TQSL" --package-version="v2.2" \
+	 --package-name="TQSL" --package-version="v2.3" \
 	 --add-comments="TRANSLATORS:" *.cpp *.h
